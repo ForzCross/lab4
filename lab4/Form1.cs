9bd22646d355da31c3f8940a6fd9d320efd83106
Forzcross <forzcross@gmail.com>
2017-03-31 05:47:16 +0300

    Добавил форму и возможность добавления картинки

--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace lab4
 {
@@ -15,6 +16,21 @@ namespace lab4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ЛР №4");
+        }
+
+        private void новоеРаспознаваниеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "bmp|*.bmp|png|*.png|jpg|*.jpg";
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Bitmap.FromFile(fileDialog.FileName);
+            }
         }
     }
 }
